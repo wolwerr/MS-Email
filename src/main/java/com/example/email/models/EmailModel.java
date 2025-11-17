b6f518e847f3d5b72c824f9932b1cf6ddda126dd
Ricardo <ricardo@dtmm.com.br>
2025-11-17 11:56:55 -0300

    Acertando pom e Serial

--- src/main/java/com/example/email/models/EmailModel.java
+++ src/main/java/com/example/email/models/EmailModel.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -17,6 +18,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmailModel implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
